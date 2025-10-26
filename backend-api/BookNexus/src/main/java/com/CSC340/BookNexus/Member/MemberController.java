5cafa6346c763a773756192b3f98c36203191870
JosephDiku <Josephdiku336@gmail.com>
2025-10-26 18:21:39 -0400

    Added findByEmailContaining in Repo class and finished controller class. Also made minor tweaks to Service class.

--- backend-api/BookNexus/src/main/java/com/CSC340/BookNexus/Member/MemberController.java
+++ backend-api/BookNexus/src/main/java/com/CSC340/BookNexus/Member/MemberController.java
@@ -1,24 +1,48 @@
 package com.CSC340.BookNexus.Member;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
 public class MemberController {
 
-    @Autowired
-    private MemberService memberService;
+    private final MemberService memberService;
 
     @PostMapping
     public ResponseEntity<Member> createMember(@Valid @RequestBody Member member) {
         return ResponseEntity.ok(memberService.createMember(member));
     }
     
+    @PutMapping("/{memberId}")
+    public ResponseEntity<Member> updateMember(@PathVariable Long memberId, @Valid @RequestBody Member memberDetails) {
+        return ResponseEntity.ok(memberService.updateMember(memberId, memberDetails));
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<Member> getMember(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.getMemberById(memberId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Member>> getAllMembers() {
+        return ResponseEntity.ok(memberService.getAllMembers());
+    }
+
+    @GetMapping("search/email")
+    public ResponseEntity<List<Member>> searchMembersByEmail(@RequestParam String email) {
+        return ResponseEntity.ok(memberService.searchMembersByEmail(email));
+    }
+
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<Void> deleteMember(@PathVariable Long memberId) {
+        memberService.deleteMember(memberId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
