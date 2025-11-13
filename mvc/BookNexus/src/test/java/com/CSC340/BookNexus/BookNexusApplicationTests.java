92c660c00b0bb93be050f9afd5a4799ada0e7ed8
Logan Wilkins <lswilkins@uncg.edu>
2025-11-13 12:40:00 -0500

    mvc folder setup

---
+++ mvc/BookNexus/src/test/java/com/CSC340/BookNexus/BookNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSC340.BookNexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
